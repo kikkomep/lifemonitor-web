fda1a05cc535870d23f669d8a4d772be35b909d6
Marco Enrico Piras <kikkomep@crs4.it>
2021-03-12 11:19:39 +0100

    Handle redirect after login within RouterModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { AppService } from 'src/app/utils/services/app.service';
+import { Routes, RouterModule, Router } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { BlankComponent } from './views/blank/blank.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ProfileComponent } from './views/profile/profile.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { AuthGuard } from './utils/guards/auth.guard';
 import { NonAuthGuard } from './utils/guards/non-auth.guard';
+import { Subscription } from 'rxjs';
 
 const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [NonAuthGuard],
+  },
   {
     path: '',
     component: MainComponent,
@@ -25,7 +33,7 @@ const routes: Routes = [
         component: BlankComponent,
       },
       {
-        path: '',
+        path: 'dashboard',
         component: DashboardComponent,
       },
     ],
@@ -40,11 +48,45 @@ const routes: Routes = [
     component: RegisterComponent,
     canActivate: [NonAuthGuard],
   },
-  { path: '**', redirectTo: '' },
+  { path: '**', redirectTo: 'login' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+  userLoggedSubscription: Subscription;
+
+  constructor(private appService: AppService, private router: Router) {
+    this.userLoggedSubscription = this.appService
+      .checkUserLogged()
+      .subscribe((userLogged) => {
+        if (userLogged) {
+          console.info('User logged... redirecting');
+          this.handleRedirect('/dashboard');
+        } else {
+          this.handleRedirect('/home');
+          console.info('User logout... redirecting');
+        }
+      });
+  }
+
+  handleRedirect(redirectTo: string = null) {
+    console.info('Current router state', this.router.routerState);
+    // get return url from route parameters or default to '/'
+    let returnUrl =
+      this.router.routerState.snapshot.root.queryParams['returnUrl'] ||
+      redirectTo ||
+      '/';
+    console.log('ReturnURL: ', returnUrl);
+    if (returnUrl) {
+      this.router.navigateByUrl(returnUrl);
+    }
+  }
+
+  ngOnDestroy() {
+    // unsubscribe to ensure no memory leaks
+    this.userLoggedSubscription.unsubscribe();
+  }
+}
