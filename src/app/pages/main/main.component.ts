21e6eaea1d399dc5e5cae6b9604fcd494f34dc31
Marco Enrico Piras <kikkomep@crs4.it>
2021-08-24 11:27:52 +0200

    Update home dashboard

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { NavigationStart, NavigationEnd, Router } from '@angular/router';
+import { filter } from "rxjs/operators";
+import { AppService } from 'src/app/utils/services/app.service';
+import { AuthService } from 'src/app/utils/services/auth.service';
 
 @Component({
   selector: 'app-main',
@@ -8,8 +12,27 @@ import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
 export class MainComponent implements OnInit {
   public sidebarMenuOpened = true;
   @ViewChild('contentWrapper', { static: false }) contentWrapper;
+  private userLoggedSubscription: any;
 
-  constructor(private renderer: Renderer2) {}
+  constructor(
+    private router: Router,
+    private renderer: Renderer2,
+    private appService: AppService,
+    private authService: AuthService
+  ) {
+
+    // this.userLoggedSubscription = this.authService
+    //   .checkUserLogged()
+    //   .subscribe((userLogged) => {
+    //     if (userLogged) {
+    //       console.info('User logged... redirecting');
+    //       this.router.navigateByUrl('/dashboard');
+    //     } else {
+    //       this.router.navigateByUrl('/');
+    //       console.info('User logout... redirecting');
+    //     }
+    //   });
+  }
 
   ngOnInit() {
     this.renderer.removeClass(document.querySelector('app-root'), 'login-page');
@@ -17,6 +40,49 @@ export class MainComponent implements OnInit {
       document.querySelector('app-root'),
       'register-page'
     );
+
+    this.router.events
+      .pipe(
+        // The "events" stream contains all the navigation events. For this demo,
+        // though, we only care about the NavigationStart event as it contains
+        // information about what initiated the navigation sequence.
+        filter((event: any) => {
+          return event instanceof NavigationStart;
+        })
+      )
+      .subscribe((event: NavigationStart) => {
+
+        console.group('NavigationStart Event');
+        console.log("event", event);
+        // Every navigation sequence is given a unique ID. Even "popstate"
+        // navigations are really just "roll forward" navigations that get
+        // a new, unique ID.
+        console.log('navigation id:', event.id);
+        console.log('route:', event.url);
+        // The "navigationTrigger" will be one of:
+        // --
+        // - imperative (ie, user clicked a link).
+        // - popstate (ie, browser controlled change such as Back button).
+        // - hashchange
+        // --
+        // NOTE: I am not sure what triggers the "hashchange" type.
+        console.log('trigger:', event.navigationTrigger);
+        // This "restoredState" property is defined when the navigation
+        // event is triggered by a "popstate" event (ex, back / forward
+        // buttons). It will contain the ID of the earlier navigation event
+        // to which the browser is returning.
+        // --
+        // CAUTION: This ID may not be part of the current page rendering.
+        // This value is pulled out of the browser; and, may exist across
+        // page refreshes.
+        if (event.restoredState) {
+          console.warn(
+            'restoring navigation id:',
+            event.restoredState.navigationId
+          );
+        }
+        console.groupEnd();
+      });
   }
 
   mainSidebarHeight(height) {
--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,12 +1,114 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { AppService } from 'src/app/utils/services/app.service';
+import { Subscription } from 'rxjs';
+import {
+  AggregatedStatusStats,
+  AggregatedStatusStatsItem,
+} from 'src/app/models/stats.model';
+import { TestBuild } from 'src/app/models/testBuild.models';
+import { Suite } from 'src/app/models/suite.models';
+import { Router } from '@angular/router';
+import { Workflow } from 'src/app/models/workflow.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
-  constructor() {}
+export class DashboardComponent implements OnInit, OnChanges {
+  // reference to workflows stats
+  private _workflowStats: AggregatedStatusStats | null;
+  // reference to the current subscription
+  private workflowsStatsSubscription: Subscription;
+  //
+  private filteredWorkflows: AggregatedStatusStatsItem[] | null;
+  //
+  public statusFilter: string | null;
+  public workflowNameFilter: string = '';
+  public workflowSortingOrder: string = 'desc';
+
+  constructor(private appService: AppService, private router: Router) {
+    console.log('Dashboard Created!!');
+    this.workflowsStatsSubscription = this.appService.observableWorkflows.subscribe(
+      (data) => {
+        this._workflowStats = data;
+        this.filteredWorkflows = this._workflowStats.all;
+        console.log('Stats', data);
+      }
+    );
+    // reload workflows (using cache)
+    this.appService.loadWorkflows(true);
+  }
 
   ngOnInit() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('Changes', changes);
+  }
+
+  public selectTestBuild(testBuild: TestBuild) {
+    console.log('Test Build selected', testBuild);
+    this.appService
+      .selectWorkflow(testBuild.testInstance.suite.workflow.uuid)
+      .subscribe((w: Workflow) => {
+        this.appService
+          .selectTestSuite(testBuild.suite_uuid)
+          .subscribe((s: Suite) => {
+            console.log('Selected suite from wf cmp: ', s);
+            this.appService
+              .selectTestInstance(testBuild.instance.uuid)
+              .subscribe((ti) => {
+                console.log('Selected test instance from wf component', ti);
+                // this.router.navigate(['/build'], {
+                //   queryParams: {
+                //     instance_uuid: testBuild.instance.uuid,
+                //     build_id: testBuild.build_id,
+                //   },
+                // });
+                console.log(
+                  'TestBuild external link: ',
+                  testBuild.externalLink
+                );
+                window.open(testBuild.externalLink, '_blank');
+              });
+          });
+      });
+  }
+
+  public get workflowStats(): AggregatedStatusStats {
+    return this._workflowStats;
+  }
+
+  public get workflows(): AggregatedStatusStatsItem[] {
+    return this.filteredWorkflows;
+  }
+
+  public filterWorkflows($event: any, workflows: AggregatedStatusStatsItem[]) {
+    console.log($event);
+    this.filteredWorkflows = workflows;
+  }
+
+  public filterByStatus(status: string) {
+    console.log("Filter by status", status);
+    if (!this._workflowStats) return;
+    try {
+      status = status == 'any' ? 'all' : status;
+      if (status != this.statusFilter) {
+        this.filteredWorkflows = this._workflowStats[status];
+        this.statusFilter = status;
+        this.workflowNameFilter = '';
+      } else {
+        this.filteredWorkflows = this._workflowStats['all'];
+        this.statusFilter = null;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  ngOnDestroy() {
+    // prevent memory leak when component destroyed
+    this.workflowsStatsSubscription.unsubscribe();
+    console.log('Destroying dashboard component');
+  }
 }
