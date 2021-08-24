e1e10861661262a7bb04d013a2875598cb66163f
Marco Enrico Piras <kikkomep@crs4.it>
2021-08-24 11:01:58 +0200

    Add search-bar component

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'item-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss'],
+})
+export class SearchBarComponent implements OnInit {
+  _actualFilterValue: string;
+  _sortingOrder: string = 'desc';
+
+  @Input() filterValue: string;
+  @Input() sortingOrder: string;
+  @Output() filterValueChange = new EventEmitter<string>();
+  @Output() sortingOrderChange = new EventEmitter<string>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public set actualFilterValue(value: string) {
+    this._actualFilterValue = value;
+    console.log('Current filter value: ', this._actualFilterValue);
+    this.filterValueChange.emit(value);
+    console.log('Current filter value: ', this._actualFilterValue);
+  }
+
+  public get actualFilterValue(): string {
+    return this._actualFilterValue;
+  }
+
+  public get actualSortingOrder(): string {
+    return this._sortingOrder;
+  }
+
+  public changeSortingOrder() {
+    this._sortingOrder = this._sortingOrder === 'asc' ? 'desc' : 'asc';
+    this.sortingOrderChange.emit(this._sortingOrder);
+  }
+}
