b819be71d031a1227c4f08037ff6604c1cf28ec0
Marco Enrico Piras <kikkomep@crs4.it>
2021-12-15 21:21:18 +0100

    Update app service to handle index of workflow registries

--- src/app/utils/services/app.service.ts
+++ src/app/utils/services/app.service.ts
@@ -10,12 +10,17 @@ import { TestInstance } from 'src/app/models/testInstance.models';
 import { User } from 'src/app/models/user.modes';
 import { Workflow } from 'src/app/models/workflow.model';
 import { ApiService } from './api.service';
+import { Registry, RegistryWorkflow } from 'src/app/models/registry.models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
   // internal state
+  private _registry: Registry;
+  private _registries: Registry[];
+  private _registryWorkflow: RegistryWorkflow;
+  private _registryWorkflows: { [uuid: string]: RegistryWorkflow[] } = {};
   private _workflowsStats: AggregatedStatusStats;
   private _workflows: Workflow[];
   private _workflow: Workflow;
@@ -27,6 +32,10 @@ export class AppService {
   private loadingWorkflows = false;
 
   // initialize data sources
+  private subjectRegistry = new Subject<Registry>();
+  private subjectRegistries = new Subject<Registry[]>();
+  private subjectRegistryWorkflow = new Subject<RegistryWorkflow>();
+  private subjectRegistryWorkflows = new Subject<RegistryWorkflow[]>();
   private subjectWorkflows = new Subject<AggregatedStatusStats>();
   private subjectWorkflow = new Subject<Workflow>();
   private subjectTestSuite = new Subject<Suite>();
@@ -35,6 +44,10 @@ export class AppService {
   private subjectLoadingWorkflows = new Subject<boolean>();
 
   // initialize data observables
+  private _observableRegistry = this.subjectRegistry.asObservable();
+  private _observableRegistries = this.subjectRegistries.asObservable();
+  private _observableRegistryWorkflow = this.subjectRegistryWorkflow.asObservable();
+  private _observableRegistryWorkflows = this.subjectRegistryWorkflows.asObservable();
   private _observableWorkflows = this.subjectWorkflows.asObservable();
   private _observableWorkflow = this.subjectWorkflow.asObservable();
   private _observableTestSuite = this.subjectTestSuite.asObservable();
@@ -129,6 +142,32 @@ export class AppService {
     return this._currentUser;
   }
 
+  public get registry(): Registry {
+    return this._registry;
+  }
+
+  public get registries(): Registry[] {
+    return this._registries;
+  }
+
+  public get registryWorkflow(): RegistryWorkflow {
+    return this._registryWorkflow;
+  }
+
+  public get registryWorkflows(): RegistryWorkflow[] {
+    return this.getRegistryWorkflows();
+  }
+
+  public getRegistryWorkflows(uuid: string = null): RegistryWorkflow[] {
+    let registry_uuid = uuid
+      ? uuid
+      : this._registry
+        ? this._registry.uuid
+        : null;
+    if (!registry_uuid) return null;
+    return this._registryWorkflows[registry_uuid];
+  }
+
   public get workflows(): Workflow[] {
     return this._workflows;
   }
@@ -151,6 +190,22 @@ export class AppService {
 
   public get testBuilds(): TestBuild {
     return this._testBuild;
+  }
+
+  public get observableRegistry(): Observable<Registry> {
+    return this._observableRegistry;
+  }
+
+  public get observableRegistries(): Observable<Registry[]> {
+    return this._observableRegistries;
+  }
+
+  public get observableRegistryWorkflow(): Observable<RegistryWorkflow> {
+    return this._observableRegistryWorkflow;
+  }
+
+  public get observableRegistryWorkflows(): Observable<RegistryWorkflow[]> {
+    return this._observableRegistryWorkflows;
   }
 
   public get observableWorkflows(): Observable<AggregatedStatusStats> {
@@ -184,6 +239,55 @@ export class AppService {
 
   public unsubscribeWorkflow(w: Workflow): Observable<Workflow> {
     return this.api.unsubscribeWorkflow(w);
+  }
+
+  public loadRegistries(): Observable<Registry[]> {
+    if (!this._registries) {
+      this.api.getRegistries().subscribe((data: Registry[]) => {
+        this._registries = data;
+        this.subjectRegistries.next(this._registries);
+      });
+    }
+    return this._observableRegistries;
+  }
+
+  private findRegistryByUuid(uuid: string): Registry {
+    if (!this._registries) return null;
+    return this._registries.find(e => e.uuid === uuid);
+  }
+
+  public selectRegistry(uuid: string, useCache: boolean = true) {
+    console.log('Selecting registry ', uuid);
+    this._registry = this.findRegistryByUuid(uuid);
+    this.subjectRegistry.next(this._registry);
+    if (this._registry) {
+      if (uuid in this._registryWorkflows && useCache) {
+        console.log('Using cached workflows');
+        this.subjectRegistryWorkflows.next(this._registryWorkflows[uuid]);
+      } else {
+        this.api.getRegistryWorkflows(uuid).subscribe((data: RegistryWorkflow[]) => {
+          console.log('Loaded registry workflows...', data);
+          let sorted = data.sort((a, b) => {
+            return a.identifier.localeCompare(b.identifier, undefined, {
+              numeric: true,
+              sensitivity: 'base'
+            })
+          })
+          this._registryWorkflows[uuid] = sorted;
+          this.subjectRegistryWorkflows.next(sorted);
+        });
+      }
+    }
+  }
+
+  public selectRegistryWorkflow(workflow_identifier: string) {
+    // TODO: enable caching
+    this.api.getRegistryWorkflow(this.registry.uuid, workflow_identifier)
+      .subscribe((w: RegistryWorkflow) => {
+        console.log("Loaded registry workflow data", w);
+        this._registryWorkflow = w;
+        this.subjectRegistryWorkflow.next(w);
+      });
   }
 
   loadWorkflows(
