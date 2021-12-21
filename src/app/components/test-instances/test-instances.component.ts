9682e95e6b850051de27080705136bd5aa3b7e9d
Marco Enrico Piras <kikkomep@crs4.it>
2021-12-21 18:30:32 +0100

    Enable pagination of test instances

--- src/app/components/test-instances/test-instances.component.ts
+++ src/app/components/test-instances/test-instances.component.ts
@@ -3,6 +3,7 @@ import {
   Component,
   EventEmitter,
   Input,
+  OnChanges,
   OnInit,
   Output,
   SimpleChanges
@@ -13,15 +14,19 @@ import { TestInstance } from 'src/app/models/testInstance.models';
 import { Workflow } from 'src/app/models/workflow.model';
 import { AppService } from 'src/app/utils/services/app.service';
 
+declare var $: any;
+
 @Component({
   selector: 'test-instances',
   templateUrl: './test-instances.component.html',
   styleUrls: ['./test-instances.component.scss'],
 })
-export class TestInstancesComponent implements OnInit {
+export class TestInstancesComponent implements OnInit, OnChanges {
   @Input() workflow: Workflow;
   @Input() testInstances: TestInstance[];
   @Output() suiteSelected = new EventEmitter<TestInstance>();
+
+  private suiteInstancesDataTable: any;
 
   constructor(
     private appService: AppService,
@@ -30,6 +35,10 @@ export class TestInstancesComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.initDataTable();
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log('Change detected');
@@ -49,6 +58,39 @@ export class TestInstancesComponent implements OnInit {
       this.appService.selectWorkflow(
         testBuild.testInstance.suite.workflow.uuid
       );
+    }
+  }
+
+  private refreshDataTable() {
+    this.destroyDataTable();
+    this.initDataTable();
+  }
+
+  private initDataTable() {
+    if (this.suiteInstancesDataTable) return;
+    this.suiteInstancesDataTable = $("#suiteInstances").DataTable({
+      "paging": true,
+      "lengthChange": true,
+      "lengthMenu": [5, 10, 20, 50, 75, 100],
+      "searching": true,
+      "ordering": true,
+      "order": [[1, 'asc']],
+      "columnDefs": [{
+        "targets": [0, 3, 4],
+        "orderable": false
+      }],
+      "info": true,
+      "autoWidth": true,
+      "responsive": true,
+      "deferRender": true,
+      stateSave: true
+    });
+  }
+
+  private destroyDataTable() {
+    if (this.suiteInstancesDataTable) {
+      this.suiteInstancesDataTable.destroy();
+      this.suiteInstancesDataTable = null;
     }
   }
 }
