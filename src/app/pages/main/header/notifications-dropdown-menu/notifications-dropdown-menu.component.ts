7c1378f874c6e8a6036f8c4f0584f4e468b669b4
Marco Enrico Piras <kikkomep@crs4.it>
2022-01-27 19:42:10 +0100

    Update DropdownMenu component to support notifications

--- src/app/pages/main/header/notifications-dropdown-menu/notifications-dropdown-menu.component.ts
+++ src/app/pages/main/header/notifications-dropdown-menu/notifications-dropdown-menu.component.ts
@@ -1,11 +1,10 @@
+import { formatDate } from '@angular/common';
 import {
-  Component,
-  OnInit,
-  ViewChild,
-  HostListener,
-  ElementRef,
-  Renderer2,
+  Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild
 } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserNotification } from 'src/app/models/notification.model';
+import { AppService } from 'src/app/utils/services/app.service';
 
 @Component({
   selector: 'app-notifications-dropdown-menu',
@@ -22,23 +21,102 @@ export class NotificationsDropdownMenuComponent implements OnInit {
     }
   }
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit() {}
+  public notifications: UserNotification[];
+  private notificationsByDate: {};
+
+  constructor(
+    private router: Router,
+    private elementRef: ElementRef,
+    private renderer: Renderer2,
+    private appService: AppService) { }
+
+  ngOnInit() {
+    this.appService.loadNotifications().subscribe((data: UserNotification[]) => {
+      console.log("loaded notifications...", data);
+      this.updateNotifications(data);
+    })
+  }
+
+  private updateNotifications(notifications: UserNotification[]) {
+    console.log("Updating notifications...", notifications);
+    this.notifications = notifications;
+    this.notificationsByDate = this.groupNotificationsByDate(notifications);
+    console.log("Notifications by date: ", this.notifications);
+  }
+
+  public get notificationsToRead(): UserNotification[] {
+    if (!this.notifications) return [];
+    return this.notifications.filter((n: UserNotification) => !n.read);
+  }
+
+  public get notificationDates() {
+    return Object.keys(this.notificationsByDate);
+  }
+
+  public getNotificationOfDay(day: string) {
+    return day in this.notificationsByDate ? this.notificationsByDate[day] : [];
+  }
+
+  private groupNotificationsByDate(notifications: UserNotification[]) {
+    let result = {};
+    for (let n of notifications) {
+      let date = formatDate(n.created, 'yyyy-MM-dd', 'en-US');
+      if (!(date in result)) {
+        result[date] = [];
+      }
+      result[date].push(n);
+    }
+    return result;
+  }
+
+  public setNotificationAsRead(n: UserNotification) {
+    this.appService.setNotificationsReadingTime([n]).subscribe((data) => {
+      console.log("Notification marked as read", n);
+      this.hideDropdownMenu();
+    });
+  }
+
+
+  public markAllAsRead(notifications: UserNotification[] = null) {
+    this.appService.setNotificationsReadingTime(notifications ? notifications : this.notifications)
+      .subscribe((data) => {
+        console.log("Notifiations marked as read", notifications);
+        this.hideDropdownMenu();
+      });
+  }
+
+  public deleteAllNotifications() {
+    this.appService.deleteNotifications(this.notifications)
+      .subscribe(() => {
+        console.log("All notifications deleted", this.notifications);
+        this.updateNotifications([]);
+        this.hideDropdownMenu();
+      });
+  }
+
+
+  public navigateTo(url: string, params: object) {
+    return this.router.navigate([url, params]);
+  }
 
   toggleDropdownMenu() {
-    if (this.dropdownMenu.nativeElement.classList.contains('show')) {
-      this.hideDropdownMenu();
-    } else {
-      this.showDropdownMenu();
+    if (this.dropdownMenu) {
+      if (this.dropdownMenu.nativeElement.classList.contains('show')) {
+        this.hideDropdownMenu();
+      } else {
+        this.showDropdownMenu();
+      }
     }
   }
 
   showDropdownMenu() {
-    this.renderer.addClass(this.dropdownMenu.nativeElement, 'show');
+    if (this.dropdownMenu)
+      this.renderer.addClass(this.dropdownMenu.nativeElement, 'show');
   }
 
   hideDropdownMenu() {
-    this.renderer.removeClass(this.dropdownMenu.nativeElement, 'show');
+    if (this.dropdownMenu)
+      this.renderer.removeClass(this.dropdownMenu.nativeElement, 'show');
   }
 }
