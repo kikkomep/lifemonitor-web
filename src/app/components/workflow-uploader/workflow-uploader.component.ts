24edee3ab32123fe6b347cdee55b2b03db9c0280
Marco Enrico Piras <kikkomep@crs4.it>
2022-01-31 18:31:28 +0100

    Fix reset of input field for local files

--- src/app/components/workflow-uploader/workflow-uploader.component.ts
+++ src/app/components/workflow-uploader/workflow-uploader.component.ts
@@ -504,6 +504,7 @@ export class WorkflowUploaderComponent
     this._workflowROCrate = null;
     this.roCrateURL = new UrlValue(this.httpClient);
     this._registrationError = null;
+    $("#roCrateInputFile").val("");
     $("#radioPrimary1").prop("checked", true);
     $("#radioPrimary2").prop("checked", false);
     $("#radioPrimary3").prop("checked", false);
