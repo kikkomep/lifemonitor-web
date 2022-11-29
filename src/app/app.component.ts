bc6cb9d4ead89aed488aed5753c572b632be1af7
Marco Enrico Piras <kikkomep@crs4.it>
2022-11-29 14:32:58 +0100

    fix(ui): set default overlay mode to 'overlay'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.primengConfig.ripple = true;
     this.primengConfig.overlayOptions = {
-      mode: 'modal',
+      mode: 'overlay',
     };
   }
 }
