1abfd80a2e3c748f04240bc239c3ea81b4ba2fee
Marco Enrico Piras <kikkomep@crs4.it>
2022-12-05 16:57:33 +0100

    feat(core): add skeleton of Workflow model

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
 
-
+import * as fromWorkflow from './reducers/workflow.reducer';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    StoreModule.forFeature(
+      fromWorkflow.workflowsFeatureKey,
+      fromWorkflow.reducer
+    ),
+  ],
 })
-export class CoreModule { }
+export class CoreModule {}
