cdca0880eaa6e91bf71f71895c5eb61cc55f6186
Marco Enrico Piras <kikkomep@crs4.it>
2022-12-06 17:04:54 +0100

    build: import missing required HttpModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,11 +5,14 @@ import { StoreModule } from '@ngrx/store';
 // Import core state
 import * as fromState from './state';
 import * as fromWorkflow from './reducers/workflow.reducer';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   imports: [
+    // core Angular modules
     CommonModule,
+    HttpClientModule,
     // register core store
     StoreModule.forFeature(fromState.stateFeatureKey, fromState.reducers, {
       metaReducers: fromState.metaReducers,
