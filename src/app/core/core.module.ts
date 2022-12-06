e8029c72b5178476a405a7cf87281ceaa1e4f2c7
Marco Enrico Piras <kikkomep@crs4.it>
2022-12-06 17:01:37 +0100

    feat(state): initialize and register global state

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,16 +2,20 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 
+// Import core state
+import * as fromState from './state';
 import * as fromWorkflow from './reducers/workflow.reducer';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    StoreModule.forFeature(
-      fromWorkflow.workflowsFeatureKey,
-      fromWorkflow.reducer
-    ),
+    // register core store
+    StoreModule.forFeature(fromState.stateFeatureKey, fromState.reducers, {
+      metaReducers: fromState.metaReducers,
+    }),
   ],
+  providers: [],
+  exports: [],
 })
 export class CoreModule {}
