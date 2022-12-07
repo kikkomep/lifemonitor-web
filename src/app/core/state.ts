a5064c86d1c333e83353aaacf9007fd1b7f58e3e
Marco Enrico Piras <kikkomep@crs4.it>
2022-12-07 17:23:35 +0100

    feat(state): combine Config and Workflows states

--- src/app/core/state.ts
+++ src/app/core/state.ts
@@ -6,14 +6,29 @@ import {
   createSelector,
   MetaReducer,
 } from '@ngrx/store';
-import { reducer as workflowsReducers } from './reducers/workflow.reducer';
+
+// Import 'Config' state and reducer
+import {
+  reducer as configReducer,
+  State as ConfigState,
+} from './reducers/config.reducer';
+
+// Import 'Workflows' state and reducer
+import {
+  reducer as workflowsReducer,
+  State as WorkflowState,
+} from './reducers/workflow.reducer';
 
 export const stateFeatureKey = 'state';
 
 export interface State {
-  workflowState: State;
+  config: ConfigState;
+  workflows: WorkflowState;
 }
 
-export const reducers: ActionReducerMap<State> = {};
+export const reducers: ActionReducerMap<State> = {
+  config: configReducer,
+  workflows: workflowsReducer,
+};
 
 export const metaReducers: MetaReducer<State>[] = isDevMode() ? [] : [];
