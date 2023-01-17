3e1dadbb500764cf3e546fa78eab1aa21f2c73f2
Marco Enrico Piras <kikkomep@crs4.it>
2023-01-17 17:00:35 +0100

    feat(ui): enable browse button on search bar

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,12 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { fromEvent } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 import { Logger, LoggerManager } from 'src/app/utils/logging';
@@ -12,43 +20,67 @@ export class SearchBarComponent implements OnInit {
   _actualFilterValue: string;
   _sortingOrder: string = 'desc';
   _searchAllValues: boolean = false;
+  browseButtonEnabled: boolean = false;
 
-  @Input() showAllButtonName: string;
-  @Input() browseButton: boolean = true;
+  @Input() goBackButtonName: string;
+  @Input() enableBrowseButton: string;
+  @Input() searchButtonName: string = 'Search';
+  @Input() allowEmptySearch: boolean = false;
   @Input() elementType: string;
   @Input() filterValue: string;
   @Input() sortingOrder: string;
+  @Output() browseEnabled = new EventEmitter<boolean>();
   @Output() filterValueChange = new EventEmitter<string>();
   @Output() sortingOrderChange = new EventEmitter<string>();
   @ViewChild('searchInputText') searchInputText: ElementRef;
+  @ViewChild('searchButton') searchButton: ElementRef;
 
   // initialize logger
   private logger: Logger = LoggerManager.create('SearchBarComponent');
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   ngAfterViewInit() {
     fromEvent(this.searchInputText.nativeElement, 'input')
       .pipe(map((event: Event) => (event.target as HTMLInputElement).value))
       .pipe(debounceTime(800))
       .pipe(distinctUntilChanged())
-      .subscribe(data => {
+      .subscribe((data) => {
         this.emitValue(data);
         this.logger.debug('Current filter value: ', this._actualFilterValue);
       });
   }
 
   public onKeyUpEnter() {
+    if (!this.actualFilterValue && !this.allowEmptySearch) return;
     this._searchAllValues = true;
     this.emitValue(this.actualFilterValue);
   }
 
+  public updateTooltips() {
+    const title: string = !this.actualFilterValue
+      ? 'Browse registered ' + this.elementType
+      : 'Search ' + this.elementType + ' by UUID or name';
+    this.searchButton.nativeElement.setAttribute('data-original-title', title);
+  }
+
+  public onBrowseButtonClick($event) {
+    this.browseButtonEnabled = !this.browseButtonEnabled;
+    this.logger.debug('OnShowAllChange', $event, this.browseButtonEnabled);
+    this.browseEnabled.emit(this.browseButtonEnabled);
+    if (this.browseButtonEnabled) {
+      this._searchAllValues = true;
+      this.emitValue(this.actualFilterValue);
+    } else {
+      this.reset();
+    }
+  }
+
   public set actualFilterValue(value: string) {
     this._actualFilterValue = value;
-    if (value && value.length > 0)
-      this._searchAllValues = false;
+    if (value && value.length > 0) this._searchAllValues = false;
     this.logger.debug('Current filter value: ', this._actualFilterValue);
   }
 
@@ -66,9 +98,12 @@ export class SearchBarComponent implements OnInit {
   }
 
   private emitValue(value: string) {
-    let valueToEmit = (this.actualFilterValue && this.actualFilterValue.length > 0)
-      ? ("SEARCH_KEY###" + this.actualFilterValue) : "______ALL_____";
+    let valueToEmit =
+      this.actualFilterValue && this.actualFilterValue.length > 0
+        ? 'SEARCH_KEY###' + this.actualFilterValue
+        : '______ALL_____';
     this.filterValueChange.emit(valueToEmit);
+    this.updateTooltips();
     this.logger.debug('Current emitted value: ', valueToEmit);
   }
 
@@ -77,11 +112,14 @@ export class SearchBarComponent implements OnInit {
   }
 
   public reset() {
-    if (this._searchAllValues === true
-      || this._actualFilterValue && this._actualFilterValue.length > 0) {
+    if (
+      this._searchAllValues === true ||
+      (this._actualFilterValue && this._actualFilterValue.length > 0)
+    ) {
       this._actualFilterValue = null;
       this._searchAllValues = false;
       this.filterValueChange.emit(null);
     }
+    this.updateTooltips();
   }
 }
