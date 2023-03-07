05e5b4722bd32b200c1cafb504b67962ccf07f8a
Marco Enrico Piras <kikkomep@crs4.it>
2023-03-07 11:48:51 +0100

    style: clean up

--- src/app/utils/services/cache/cache-manager.ts
+++ src/app/utils/services/cache/cache-manager.ts
@@ -475,15 +475,3 @@ export class CacheManager {
     return false;
   }
 }
-
-// if (typeof Worker !== 'undefined') {
-//   // Create a new
-//   const worker = new Worker(new URL('./cache.worker', import.meta.url));
-//   worker.onmessage = ({ data }) => {
-//     console.log(`page got message: ${data}`);
-//   };
-//   worker.postMessage('hello');
-// } else {
-//   // Web Workers are not supported in this environment.
-//   // You should add a fallback so that your program still executes correctly.
-// }
