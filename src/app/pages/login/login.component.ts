ae564461e310af3c676ddd3d84043fad2d5cdd7d
Marco Enrico Piras <kikkomep@crs4.it>
2023-03-30 11:53:56 +0200

    fix: restore redirect to /dashboard after login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AppService } from 'src/app/utils/services/app.service';
 
 import { ActiveToast, ToastrService } from 'ngx-toastr';
@@ -23,6 +23,7 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   constructor(
     private renderer: Renderer2,
+    private router: Router,
     private toastr: ToastrService,
     private appService: AppService,
     private activatedRoute: ActivatedRoute
@@ -45,6 +46,7 @@ export class LoginComponent implements OnInit, OnDestroy {
         });
       } else {
         this.appService.login().then((user) => {
+          this.router.navigateByUrl('/dashboard');
           this.previousToast = this.toastr.success(
             'You have successfully <b>logged in</b>',
             `Hi, ${user.username}`,
