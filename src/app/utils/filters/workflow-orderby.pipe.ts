8c67fb362d28e0006423fcdb952ddc06b080477b
Marco Enrico Piras <kikkomep@crs4.it>
2023-03-31 11:16:20 +0200

    fix(pipe): always use the latest version that has been computed

--- src/app/utils/filters/workflow-orderby.pipe.ts
+++ src/app/utils/filters/workflow-orderby.pipe.ts
@@ -1,9 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Workflow } from 'src/app/models/workflow.model';
 
 @Pipe({
   name: 'workflowOrderBy',
 })
 export class WorkflowOrderByPipe implements PipeTransform {
+  private LATEST_VERSION_KEY = '__latest_version__';
+
   transform(
     array: any[],
     property: string,
@@ -18,15 +21,22 @@ export class WorkflowOrderByPipe implements PipeTransform {
     }
 
     const sortedArray = array.sort((a, b) => {
-      const aValue = this.getLatestModified(a.workflow.versions, property);
-      const bValue = this.getLatestModified(b.workflow.versions, property);
+      const aValue = this.getLatestModified(a.workflow, property);
+      const bValue = this.getLatestModified(b.workflow, property);
       return this.compare(aValue, bValue, direction);
     });
 
     return sortedArray;
   }
 
-  getLatestModified(versions: any[], property: string): number {
+  getLatestModified(workflow: any, property: string): number {
+    if (!workflow || !(workflow instanceof Workflow)) return 0;
+
+    // Use the previously found latest version if it exists
+    if (workflow[this.LATEST_VERSION_KEY])
+      return workflow[this.LATEST_VERSION_KEY].modified;
+
+    const versions: any[] = workflow.versions;
     if (!Array.isArray(versions) || versions.length === 0) {
       return 0;
     }
@@ -36,6 +46,7 @@ export class WorkflowOrderByPipe implements PipeTransform {
       const b = current[property] || 0;
       return this.compare(a, b, 'desc');
     });
+    workflow[this.LATEST_VERSION_KEY] = latestVersion;
     return latestVersion.modified;
   }
 
@@ -43,7 +54,7 @@ export class WorkflowOrderByPipe implements PipeTransform {
     if (typeof aValue === 'number' && typeof bValue === 'number') {
       return (aValue - bValue) * (direction === 'asc' ? 1 : -1);
     } else {
-      const comparison = aValue.localeCompare(bValue);
+      const comparison = aValue ? aValue.localeCompare(bValue) : bValue;
       const result = comparison * (direction === 'asc' ? 1 : -1);
       return result;
     }
