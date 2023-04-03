cd4ec664c2db5655708257f88f14ff76dc02b84c
Marco Enrico Piras <kikkomep@crs4.it>
2023-04-03 16:46:20 +0200

    feat(ui): notify new app version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
-import { CachedHttpClientService } from './utils/services/cache/cachedhttpclient.service';
+
+import { Logger, LoggerManager } from './utils/logging/index';
+import { InputDialogService } from './utils/services/input-dialog.service';
 
 declare var $: any;
 
@@ -9,22 +11,32 @@ declare var $: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'LifeMonitor';
   webWorker: Worker;
+  private logger: Logger = LoggerManager.create('AppComponent');
 
   constructor(
-    private swUpdate: SwUpdate,
-    private client: CachedHttpClientService
-  ) { }
+    private inputDialog: InputDialogService,
+    private swUpdate: SwUpdate
+  ) {}
 
   ngOnInit() {
-
     if (this.swUpdate.isEnabled) {
       this.swUpdate.available.subscribe(() => {
-        if (confirm("New version available. Load New Version?")) {
+        this.inputDialog.show({
+          iconImage: '/assets/img/logo/lm/LifeMonitorLogo.png',
+          iconImageSize: '250',
+          question: "<span class='text-primary'>new version available</span>",
+          description: '... restarting ...',
+          enableCancel: false,
+          onConfirm: null,
+          enableClose: false,
+        });
+
+        setTimeout(() => {
           window.location.reload();
-        }
+        }, 2000);
       });
     }
   }
@@ -44,4 +56,6 @@ export class AppComponent implements OnInit {
         $(this).tooltip('hide');
       });
   }
+
+  ngOnDestroy() {}
 }
