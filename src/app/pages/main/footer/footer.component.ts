c08bfba790f7e6e8fb62f0e0fc6cdf492e929b29
Marco Enrico Piras <kikkomep@crs4.it>
2023-05-26 15:34:39 +0200

    refactor: update links to legal docs on footer

--- src/app/pages/main/footer/footer.component.ts
+++ src/app/pages/main/footer/footer.component.ts
@@ -10,9 +10,13 @@ import packageInfo from '../../../../../package.json';
 export class FooterComponent implements OnInit {
   public appVersion = packageInfo.version;
   public backendBaseUrl: string = null;
+  public privacyPolicyUrl: string = null;
+  public termsOfServiceUrl: string = null;
   constructor(private appConfig: AppConfigService) {
     this.backendBaseUrl = this.appConfig.apiBaseUrl;
+    this.privacyPolicyUrl = this.appConfig.privacyPolicyUrl;
+    this.termsOfServiceUrl = this.appConfig.termsOfServiceUrl;
   }
 
-  ngOnInit() { }
+  ngOnInit() {}
 }
